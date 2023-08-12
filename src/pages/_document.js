2b33b712eaa685bc943a65820561599495c4dc36
Ömer Asaf Karasu <omerconsept999@gmail.com>
2023-08-13 00:30:46 +0300

    Metadata updated

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Ömer Asaf - Personel Portfolio</title>
+        <meta name="description" content="Hello, This my portfolio web site, if you want web developer ore full stack developer you should preview my web site" key="desc" />
+      </Head>
       <body>
         <Main />
         <NextScript />
