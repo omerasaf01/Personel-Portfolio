f0706cda88619c7c3dbb1cff683333d6aa6a2082
Ömer Asaf Karasu <omerconsept999@gmail.com>
2023-08-12 23:44:47 +0300

    Skills component widths fixed

--- src/components/skills/skills.jsx
+++ src/components/skills/skills.jsx
@@ -34,7 +34,7 @@ export default function Skills() {
             <SiDotnet className="text-purple-500 w-[55px] h-[55px]" />
             <h3 className="text-blue-500">.Net Core</h3>
           </button>
-          <button className="border border-gray-800 rounded-2xl flex justify-between items-center px-2 hover:border-blue-700 transition-all w-[223.89px] h-[57px]">
+          <button className="border border-gray-800 rounded-2xl flex justify-between items-center px-2 hover:border-blue-700 transition-all  h-[57px]">
             <TbBrandNextjs className="text-gray-500 w-[45px] h-[45px]" />
             <h3 className="text-blue-500">Next.js</h3>
           </button>
@@ -42,7 +42,7 @@ export default function Skills() {
             <SiTailwindcss className="text-blue-400 w-[55px] h-[55px]" />
             <h3 className="text-blue-500">Tailwind.css</h3>
           </button>
-          <button className="border border-gray-800 rounded-2xl flex justify-between items-center px-2 hover:border-blue-700 transition-all w-[223.89px] h-[57px]">
+          <button className="border border-gray-800 rounded-2xl flex justify-between items-center px-2 hover:border-blue-700 transition-all  h-[57px]">
             <SiJira className="text-blue-500 w-[35px] h-[35px]" />
             <h3 className="text-blue-500">Jira</h3>
           </button>
@@ -62,31 +62,31 @@ export default function Skills() {
             <DiReact className="text-blue-500 w-[55px] h-[55px]" />
             <h3 className="text-blue-500">React Native</h3>
           </button>
-          <button className="border border-gray-800 rounded-2xl flex justify-between items-center px-2 hover:border-blue-700 transition-all w-[223.89px] h-[57px]">
+          <button className="border border-gray-800 rounded-2xl flex justify-between items-center px-2 hover:border-blue-700 transition-all  h-[57px]">
             <SiElectron className="text-gray-500 w-[45px] h-[45px]" />
             <h3 className="text-blue-500">Electron.js</h3>
           </button>
-          <button className="border border-gray-800 rounded-2xl flex justify-between items-center px-2 hover:border-blue-700 transition-all w-[223.89px] h-[57px]">
+          <button className="border border-gray-800 rounded-2xl flex justify-between items-center px-2 hover:border-blue-700 transition-all  h-[57px]">
             <DiLinux className="text-white w-[45px] h-[45px]" />
             <h3 className="text-blue-500">Linux</h3>
           </button>
-          <button className="border border-gray-800 rounded-2xl flex justify-between items-center px-2 hover:border-blue-700 transition-all w-[223.89px] h-[57px]">
+          <button className="border border-gray-800 rounded-2xl flex justify-between items-center px-2 hover:border-blue-700 transition-all  h-[57px]">
             <AiFillGithub className="text-white w-[45px] h-[45px]" />
             <h3 className="text-blue-500">Github</h3>
           </button>
-          <button className="border border-gray-800 rounded-2xl flex justify-between items-center px-2 hover:border-blue-700 transition-all w-[223.89px] h-[57px]">
+          <button className="border border-gray-800 rounded-2xl flex justify-between items-center px-2 hover:border-blue-700 transition-all  h-[57px]">
             <IoLogoPython className="w-[45px] h-[45px] text-yellow-400" />
             <h3 className="text-blue-500">Python</h3>
           </button>
-          <button className="border border-gray-800 rounded-2xl flex justify-between items-center px-2 hover:border-blue-700 transition-all w-[223.89px] h-[57px]">
+          <button className="border border-gray-800 rounded-2xl flex justify-between items-center px-2 hover:border-blue-700 transition-all  h-[57px]">
             <SiFlask className="w-[45px] h-[45px] text-gray-200" />
             <h3 className="text-blue-500">Flask</h3>
           </button>
-          <button className="border border-gray-800 rounded-2xl flex justify-between items-center px-2 hover:border-blue-700 transition-all w-[223.89px] h-[57px]">
+          <button className="border border-gray-800 rounded-2xl flex justify-between items-center px-2 hover:border-blue-700 transition-all  h-[57px]">
             <TbSql className="text-white w-[45px] h-[45px]" />
             <h3 className="text-blue-500">Sql</h3>
           </button>
-          <button className="border border-gray-800 rounded-2xl flex justify-between items-center px-2 hover:border-blue-700 transition-all w-[223.89px] h-[57px]">
+          <button className="border border-gray-800 rounded-2xl flex justify-between items-center px-2 hover:border-blue-700 transition-all  h-[57px]">
             <SiTypescript className="text-blue-400 w-[35px] h-[35px]" />
             <h3 className="text-blue-500">TypeScript</h3>
           </button>
