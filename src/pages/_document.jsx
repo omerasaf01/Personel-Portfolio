dea082c37e9ff2d435f7a94d5b03617ab259bdd0
Ömer Asaf Karasu <omerconsept999@gmail.com>
2023-12-10 00:30:05 +0300

    Seo

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -4,6 +4,8 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <title>Ömer Asaf Karasu - Portfolio</title>
+        <meta name="description" content="This my personel web site, if you look or search full stack developer, you can contact me." />
       </Head>
       <body className="scroll-smooth">
         <Main />
